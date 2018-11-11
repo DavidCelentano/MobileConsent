a869a3bf8156d2d87d4418f6e0d713a91db88fa5
David Celentano <davidwcelentano@gmail.com>
2018-11-10 19:51:57 -0500

    Initial Commit

---
+++ MobileConsent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileConsent
+//
+//  Created by David Celentano on 11/10/18.
+//  Copyright © 2018 David Celentano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
